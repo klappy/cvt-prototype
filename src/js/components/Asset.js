382bcc3ac39f886f441515bef899207d40b2e956
Christopher Klapp <christopher@klapp.name>
2017-12-13 18:22:55 -0500

    Added order/history buy/sell badges, last trade time ago

--- src/js/components/Asset.js
+++ src/js/components/Asset.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import moment from 'moment';
 import {ListItem} from 'material-ui/List';
 import LinearProgress from 'material-ui/LinearProgress';
 import Divider from 'material-ui/Divider';
@@ -53,16 +54,42 @@ const Asset = ({
     <AssetSettings key="0" assetCode={assetCode} settings={settings} actions={actions}/>
   ];
 
+  const orderBuyCount = orders.filter((o) => (o.type === 'buy')).length;
+  const orderSellCount = orders.filter((o) => (o.type === 'sell')).length;
+  const historyBuyCount = tradeHistory.filter((o) => (o.type === 'buy')).length;
+  const historySellCount = tradeHistory.filter((o) => (o.type === 'sell')).length;
+
   const AssetIcon = AssetIcons[assetCode[0].toUpperCase() + assetCode.substring(1).replace(/\d+$/, "").toLowerCase()];
   const assetIconColor = orderRecommendationStyle.color ? orderRecommendationStyle.color : 'rgb(117, 117, 117)';
   const assetIcon = (
     <Badge
-      badgeContent={orders.length}
-      secondary={true}
-      badgeStyle={{top: 25, right: -10}}
+      badgeContent={historySellCount}
+      secondary={false}
+      badgeStyle={{top: -15, right: 31, background: 'transparent'}}
       style={{padding: 0}}
     >
-      <AssetIcon size={40} color={assetIconColor} />
+      <Badge
+        badgeContent={orderSellCount}
+        secondary={false}
+        badgeStyle={{top: -15, right: -15, background: 'transparent'}}
+        style={{padding: 0}}
+      >
+        <Badge
+          badgeContent={historyBuyCount}
+          secondary={false}
+          badgeStyle={{top: 31, right: 31, background: 'transparent'}}
+          style={{padding: 0}}
+        >
+          <Badge
+            badgeContent={orderBuyCount}
+            secondary={false}
+            badgeStyle={{top: 31, right: -15, background: 'transparent'}}
+            style={{padding: 0}}
+          >
+            <AssetIcon size={40} color={assetIconColor} />
+          </Badge>
+        </Badge>
+      </Badge>
     </Badge>
   );
 
@@ -77,6 +104,14 @@ const Asset = ({
     signal = '-';
   }
 
+  let lastTrade = "Never";
+  if (tradeHistory.length > 0) {
+    const lastTradeTime = tradeHistory[0].date;
+    const timeZoneOffset = (new Date()).getTimezoneOffset();
+    const lastTradeTimeOffset = moment(lastTradeTime).subtract(timeZoneOffset, 'minutes');
+    lastTrade = moment(lastTradeTimeOffset).fromNow();
+  }
+
   return (
     <div>
       <Divider />
@@ -84,7 +119,7 @@ const Asset = ({
         leftAvatar={assetIcon}
         primaryText={
           <span>
-            {assetCode}
+            {assetCode} - Last Trade {lastTrade}
             <LinearProgress size={22} thickness={2.5} mode="determinate" color={progressColor} value={urgentOrder.percentToTrade*100} />
           </span>
         }
